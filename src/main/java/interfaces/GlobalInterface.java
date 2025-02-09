d8124f0fab20115d89f46c91612a0b315ad156ac
Majd04bougatef <127989172+Majd04bougatef@users.noreply.github.com>
2025-02-09 22:30:10 +0100

    commit cennect db

---
+++ src/main/java/interfaces/GlobalInterface.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+import java.util.List;
+
+public interface GlobalInterface<T> {
+    void add (T t);
+    void update (T t);
+    void delete (T t);
+    List<T> getAll();
+    List<T> getById(int id);
+
+}
---
+++ src/main/java/test/Main.java
@@ -0,0 +1,17 @@
+package test;
+
+import util.MyDatabase;
+
+import java.sql.Connection;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        MyDatabase db = MyDatabase.getInstance();
+        Connection con = db.getCon();
+
+
+    }
+}
