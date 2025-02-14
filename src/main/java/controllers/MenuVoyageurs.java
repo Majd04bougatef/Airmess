6cde0f25704b583c0da32e64b79e7a7f3769103c
elaaelaa <elaa.mahmoudi@esprit.tn>
2025-02-14 20:55:56 +0100

    fix fontawesome depedency

--- src/main/java/controllers/MenuVoyageurs.java
+++ src/main/java/controllers/MenuVoyageurs.java
@@ -2,6 +2,7 @@ package controllers;
 
 
 //import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Separator;
 import javafx.scene.image.ImageView;
