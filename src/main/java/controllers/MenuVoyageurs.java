cacfe4373c2ca06e6f799b32ca753cd92e387500
Majd04bougatef <127989172+Majd04bougatef@users.noreply.github.com>
2025-02-14 20:55:02 +0100

    commit

---
+++ src/main/java/controllers/MenuVoyageurs.java
@@ -0,0 +1,42 @@
+package controllers;
+
+
+//import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXML;
+import javafx.scene.control.Separator;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+public class MenuVoyageurs {
+    private FontAwesomeIconView iconBICUCLE;
+
+    @FXML
+    private FontAwesomeIconView iconBONPLAN;
+
+    @FXML
+    private FontAwesomeIconView iconHome;
+
+    @FXML
+    private FontAwesomeIconView iconOFFRE;
+
+    @FXML
+    private FontAwesomeIconView iconSOCIALMEDIA;
+
+    @FXML
+    private FontAwesomeIconView iconUser;
+
+    @FXML
+    private Circle imageUser;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Separator seaprtor2;
+
+    @FXML
+    private Separator separtor1;
+
+    @FXML
+    private VBox vboxmenu_voyageurs;
+}
---
+++ src/main/java/controllers/test.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+
+public class test {
+    @FXML
+    private TextField id1;
+
+    @FXML
+    private TextField id2;
+}
