01ac83857853cfef8046649db2dda9ee558e07e6
Majd04bougatef <127989172+Majd04bougatef@users.noreply.github.com>
2025-02-14 22:29:57 +0100

    committt

--- src/main/java/controllers/MenuVoyageurs.java
+++ src/main/java/controllers/MenuVoyageurs.java
@@ -1,29 +1,11 @@
 package controllers;
 
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Separator;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 public class MenuVoyageurs {
-    @FXML
-    private FontAwesomeIconView iconBICUCLE;
-
-    @FXML
-    private FontAwesomeIconView iconBONPLAN;
-
-    @FXML
-    private FontAwesomeIconView iconHome;
-
-    @FXML
-    private FontAwesomeIconView iconOFFRE;
-
-    @FXML
-    private FontAwesomeIconView iconSOCIALMEDIA;
-
-    @FXML
-    private FontAwesomeIconView iconUser;
 
     @FXML
     private Circle imageUser;
