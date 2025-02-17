f08ea71fa352dc037498789fdadb3b9e49a3498c
Majd04bougatef <127989172+Majd04bougatef@users.noreply.github.com>
2025-02-17 13:11:27 +0100

    commit form add

---
+++ src/main/java/controllers/FormAddTransport.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class FormAddTransport {
+
+    @FXML
+    private TextField Description;
+
+    @FXML
+    private TextField Localistion;
+
+    @FXML
+    private TextField Titre;
+
+    @FXML
+    private Button bttn;
+
+    @FXML
+    private SplitMenuButton categorie;
+
+    @FXML
+    private Pane form;
+
+    @FXML
+    private Text text1;
+
+    @FXML
+    private Text textTitre;
+
+    @FXML
+    private Text textTitre1;
+
+    @FXML
+    private Text textTitre11;
+
+    @FXML
+    private Text textTitre2;
+
+}
