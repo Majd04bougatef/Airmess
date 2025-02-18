6adbd15d1c6d5cb839534109d5e3e322671f9b50
Majd04bougatef <127989172+Majd04bougatef@users.noreply.github.com>
2025-02-18 18:45:42 +0100

    commit

--- src/main/java/controllers/MenuVoyageurs.java
+++ src/main/java/controllers/MenuVoyageurs.java
@@ -12,55 +12,10 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-import test.Session;
+
 import java.io.IOException;
 
-
-
 public class MenuVoyageurs {
-
-
-
-
-    @FXML
-    void Home(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Offre(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Socail(ActionEvent event) {
-
-    }
-
-    @FXML
-    void User(ActionEvent event) {
-
-    }
-
-    @FXML
-    void Velo(ActionEvent event) {
-
-    }
-
-
-    @FXML
-    void BonPlan(ActionEvent event) {
-
-    }
-
-
-
-
-
 
     @FXML
     private AnchorPane anchorpane1;
@@ -69,28 +24,7 @@ public class MenuVoyageurs {
     private AnchorPane centralAnocherPane;
 
     @FXML
-    private ImageView iconBonPlan;
-
-    @FXML
-    private ImageView iconHome;
-
-    @FXML
-    private ImageView iconLogout;
-
-    @FXML
-    private ImageView iconOffre;
-
-    @FXML
     private Text iconSedeconnecter;
-
-    @FXML
-    private ImageView iconSocailMedia;
-
-    @FXML
-    private ImageView iconTransport;
-
-    @FXML
-    private ImageView iconUser;
 
     @FXML
     private Circle imageUser;
@@ -114,39 +48,22 @@ public class MenuVoyageurs {
     private Separator separtor1;
 
     @FXML
-    private Text textIconBonPlan;
-
-    @FXML
-    private Text textIconHome;
-
-    @FXML
-    private Text textIconOffre;
-
-    @FXML
-    private Text textIconSocail;
-
-    @FXML
-    private Text textIconUser;
-
-    @FXML
-    private Text textIconVelo;
-
-    @FXML
     private VBox vboxmenu_voyageurs;
 
     @FXML
-    public void initialize() {
-        iconHome.setOnMouseClicked(this::loadHomePage);
-        iconUser.setOnMouseClicked(this::loadUserPage);
-        iconTransport.setOnMouseClicked(this::loadTranportPage);
-        iconBonPlan.setOnMouseClicked(this::loadBonplanPage);
-        iconOffre.setOnMouseClicked(this::loadOffrePage);
-        iconSocailMedia.setOnMouseClicked(this::loadSocialMediaPage);
-        iconLogout.setOnMouseClicked(this::loadSocialMediaPage);
+    void BonPlan(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/test.fxml"));
+            Parent userPage = loader.load();
 
+            centralAnocherPane.getChildren().setAll(userPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void loadHomePage(MouseEvent event) {
+    @FXML
+    void Home(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/homePage.fxml"));
             Parent userPage = loader.load();
@@ -157,7 +74,8 @@ public class MenuVoyageurs {
         }
     }
 
-    private void loadUserPage(MouseEvent event) {
+    @FXML
+    void Offre(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/test.fxml"));
             Parent userPage = loader.load();
@@ -168,40 +86,8 @@ public class MenuVoyageurs {
         }
     }
 
-    private void loadTranportPage(MouseEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FormAddTransport.fxml"));
-            Parent userPage = loader.load();
-
-            centralAnocherPane.getChildren().setAll(userPage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadBonplanPage(MouseEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/test.fxml"));
-            Parent userPage = loader.load();
-
-            centralAnocherPane.getChildren().setAll(userPage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadOffrePage(MouseEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/test.fxml"));
-            Parent userPage = loader.load();
-
-            centralAnocherPane.getChildren().setAll(userPage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadSocialMediaPage(MouseEvent event) {
+    @FXML
+    void Socail(ActionEvent event) {
 
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/FormAddSocialMedia.fxml"));
@@ -212,6 +98,31 @@ public class MenuVoyageurs {
             e.printStackTrace();
         }
     }
+
+    @FXML
+    void User(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/test.fxml"));
+            Parent userPage = loader.load();
+
+            centralAnocherPane.getChildren().setAll(userPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void Velo(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FormAddTransport.fxml"));
+            Parent userPage = loader.load();
+
+            centralAnocherPane.getChildren().setAll(userPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @FXML
     void logout(ActionEvent event) {
