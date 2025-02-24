58ddc88da4f8ceeff7fa58770e31c29359a8a0ac
oussemabb20 <benelhaj.oussema@esprit.tn>
2025-02-24 11:05:37 +0100

    fxml forget password

---
+++ src/main/java/controllers/user/Passeoublié.java
@@ -0,0 +1,18 @@
+package controllers.user;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Passeoublié {
+
+    @FXML
+    private TextField email_identifier;
+
+    @FXML
+    void Rechercher(ActionEvent event) {
+
+    }
+
+}
+
